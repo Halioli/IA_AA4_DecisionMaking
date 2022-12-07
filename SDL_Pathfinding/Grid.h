326b89ac7d14369a40513ed26afdc2f3f780ed44
Aniol <aniolbarnadat@gmail.com>
2022-12-07 09:37:56 +0100

    Added most if not all of Lluís' code, edited .gitignore, linked scenes

--- SDL_Pathfinding/Grid.h
+++ SDL_Pathfinding/Grid.h
@@ -23,6 +23,7 @@ public:
 	Vector2D cell2pix(Vector2D cell);
 	Vector2D pix2cell(Vector2D pix);
 	bool isValidCell(Vector2D cell);
+	int getCellValue(Vector2D cell);
 	int getNumCellX();
 	int getNumCellY();
 	float getTerrainWeight(int i, int j);
